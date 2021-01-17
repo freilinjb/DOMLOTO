a30b281c5879caabcedf8ade6b76edaf5389bc3c
freilinjb <freilinjb@gmail.com>
2021-01-17 14:26:18 -0400

    Cambiando a nextjs y puerto del server

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const app = require('./app');
 
-app.set('port', process.env.PORT || 3000 );
+app.set('port', process.env.PORT || 4000 );
 app.listen(app.get('port'), () => {
     console.log(`Server on Port ${app.get('port')}`);
 });
